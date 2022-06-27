7625f554326f1c090cceb248b0bb03729bf595fa
fengbujue <1125238900@qq.com>
2022-06-27 20:58:13 +0800

    解决CSS文件打包问题

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import "./banner.js";
 
 import $ from "jquery";
 $("#swiper").css("background-color", "skyblue");
+
+import './style/index.css'
--- webpack.config.js
+++ webpack.config.js
@@ -20,4 +20,17 @@ module.exports = {
     port: 3000, // 端口号
     open: true,
   },
+  module: {
+    // loader 加载器 配置在这儿
+    rules: [
+      // loader的规则
+      {
+        test: /\.css$/, // 匹配所有的css文件
+        // loader 执行的顺序： use数组里从右向左运行
+        // 先用 css-loader 让webpack能够识别 css 文件的内容并打包
+        // 再用 style-loader 将样式, 把css插入到dom中
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
