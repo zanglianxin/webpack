3fdfa7cc16a171bf0f6545670182a007a51d972a
fengbujue <1125238900@qq.com>
2022-06-27 20:50:21 +0800

    添加了serve插件，生成html插件和清除文件插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
-    }
-}
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    port: 3000, // 端口号
+    open: true,
+  },
+};
